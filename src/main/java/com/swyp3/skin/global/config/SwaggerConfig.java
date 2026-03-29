2051d72b5fa1667d379177e52c5386347c210c1d
siiooin <sssyyy0716@naver.com>
2026-03-30 01:33:56 +0900

    feat: Spring Security 및 Swagger 초기 세팅 (#3)
    - SwaggerConfig: API 명세서 정보 세팅
    - Springdoc OpenAPI 라이브러리 추가
    - SecurityConfig: Swagger UI 접속 경로 개방

--- src/main/java/com/swyp3/skin/global/config/SwaggerConfig.java
+++ src/main/java/com/swyp3/skin/global/config/SwaggerConfig.java
@@ -1,4 +1,18 @@
 package com.swyp3.skin.global.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("스킨 서비스 API 명세서")
+                        .description("스킨 서비스 백엔드 API 문서입니다.")
+                        .version("1.0.0"));
+    }
 }
