e79ad80cf3b0fbf2325e0dfe6149bd7a9cbf31fa
kmsdevdata-sketch <kmsdevdata@gmail.com>
2026-04-07 15:11:03 +0900

    refactor:AuthErrorcode-implements(#29)

--- src/main/java/com/swyp3/skin/global/auth/exception/AuthErrorCode.java
+++ src/main/java/com/swyp3/skin/global/auth/exception/AuthErrorCode.java
@@ -1,12 +1,13 @@
 package com.swyp3.skin.global.auth.exception;
 
+import com.swyp3.skin.global.exception.ErrorCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
-public enum AuthErrorCode {
+public enum AuthErrorCode implements ErrorCode {
 
     INVALID_PROVIDER(HttpStatus.BAD_REQUEST, "AUTH_400_001", "지원하지 않는 소셜 로그인입니다");
 
