246f9d1b78311c787960cf83655c80b40fc256de
윤민수 <koelysa5696@naver.com>
2026-04-09 18:15:05 +0900

    feat: table 매핑 이름 추가

--- src/main/java/com/swyp3/skin/domain/user/domain/entity/User.java
+++ src/main/java/com/swyp3/skin/domain/user/domain/entity/User.java
@@ -2,22 +2,33 @@ package com.swyp3.skin.domain.user.domain.entity;
 
 import com.swyp3.skin.domain.user.domain.enums.UserRole;
 import com.swyp3.skin.global.entity.BaseEntity;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Table(name = "users")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseEntity {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private UserRole role;
 
-    private User(UserRole role){
+    private User(UserRole role) {
         this.role = role;
     }
 
