18ba37edab6f1ff6dc0f4c669577a417d24edf1b
윤민수 <koelysa5696@naver.com>
2026-04-09 18:17:07 +0900

    feat: nickname 삭제

--- src/main/java/com/swyp3/skin/domain/user/domain/entity/UserProfile.java
+++ src/main/java/com/swyp3/skin/domain/user/domain/entity/UserProfile.java
@@ -1,8 +1,16 @@
 package com.swyp3.skin.domain.user.domain.entity;
 
 import com.swyp3.skin.global.entity.BaseEntity;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
@@ -12,22 +20,19 @@ public class UserProfile extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne(fetch = FetchType.LAZY) @JoinColumn(nullable = false, unique = true)
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, unique = true)
     private User user;
-
-    @Column(nullable = false)
-    private String nickname;
 
     private String profileImageUrl;
 
 
-    private UserProfile(User user, String nickname, String profileImageUrl) {
+    private UserProfile(User user, String profileImageUrl) {
         this.user = user;
-        this.nickname = nickname;
         this.profileImageUrl = profileImageUrl;
     }
 
-    public static UserProfile create(User user, String nickname, String profileImageUrl) {
-        return new UserProfile(user, nickname, profileImageUrl);
+    public static UserProfile create(User user, String profileImageUrl) {
+        return new UserProfile(user, profileImageUrl);
     }
 }
