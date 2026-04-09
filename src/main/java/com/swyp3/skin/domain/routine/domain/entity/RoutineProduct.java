cd88979eaf24cf24bcc24d4f40aab6e75564fd8d
윤민수 <koelysa5696@naver.com>
2026-04-09 18:14:19 +0900

    feat: sortOrder null 불가 기능 추가

--- src/main/java/com/swyp3/skin/domain/routine/domain/entity/RoutineProduct.java
+++ src/main/java/com/swyp3/skin/domain/routine/domain/entity/RoutineProduct.java
@@ -2,7 +2,14 @@ package com.swyp3.skin.domain.routine.domain.entity;
 
 import com.swyp3.skin.domain.product.domain.entity.Product;
 import com.swyp3.skin.global.entity.BaseEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,10 +21,13 @@ class RoutineProduct extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
     private Routine routine;
-    @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
     private Product product;
+    @Column(nullable = false)
     private Integer sortOrder;
     private String note;
     private String reason;
