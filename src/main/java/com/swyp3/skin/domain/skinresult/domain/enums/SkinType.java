bc6bf851a4a9c94f1e2b938f7977d0ecbe1d7142
kmsdevdata-sketch <kmsdevdata@gmail.com>
2026-04-09 16:00:36 +0900

    feat:SkinType&Concern-add(#45)

--- src/main/java/com/swyp3/skin/domain/skinresult/domain/enums/SkinType.java
+++ src/main/java/com/swyp3/skin/domain/skinresult/domain/enums/SkinType.java
@@ -1,4 +1,9 @@
 package com.swyp3.skin.domain.skinresult.domain.enums;
 
-// 추후 추가예정
-public enum SkinType { DRY, OILY, COMBINATION, SENSITIVE }
+public enum SkinType {
+    DRY,          // 건성
+    OILY,         // 지성
+    COMBINATION,  // 복합성
+    SENSITIVE,    // 민감성
+    UNKNOWN       // 잘 모르겠어요
+}
