e94248c931f8dcea1bb87cb7ddb340f68b0d917d
윤민수 <koelysa5696@naver.com>
2026-04-09 19:36:35 +0900

    feat: nickname 삭제

--- src/main/java/com/swyp3/skin/global/auth/CustomOAuth2UserService.java
+++ src/main/java/com/swyp3/skin/global/auth/CustomOAuth2UserService.java
@@ -3,13 +3,14 @@ package com.swyp3.skin.global.auth;
 import com.swyp3.skin.domain.user.domain.entity.User;
 import com.swyp3.skin.domain.user.domain.entity.UserOauth;
 import com.swyp3.skin.domain.user.domain.entity.UserProfile;
-import com.swyp3.skin.global.auth.enums.AuthProvider;
 import com.swyp3.skin.domain.user.domain.enums.UserRole;
 import com.swyp3.skin.domain.user.domain.repository.UserOauthRepository;
 import com.swyp3.skin.domain.user.domain.repository.UserProfileRepository;
 import com.swyp3.skin.domain.user.domain.repository.UserRepository;
+import com.swyp3.skin.global.auth.enums.AuthProvider;
 import com.swyp3.skin.global.auth.exception.AuthErrorCode;
 import com.swyp3.skin.global.auth.exception.AuthException;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -19,12 +20,10 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Map;
-
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class  CustomOAuth2UserService extends DefaultOAuth2UserService {
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
     private final UserRepository userRepository;
     private final UserOauthRepository userOauthRepository;
@@ -90,7 +89,6 @@ public class  CustomOAuth2UserService extends DefaultOAuth2UserService {
                     // 3. UserProfile 정보 저장 (UserProfile 엔티티도 create 메서드를 만들었다고 가정)
                     userProfileRepository.save(UserProfile.create(
                             user,
-                            "SkinUser_" + user.getId(),
                             finalProfileImageUrl
                     ));
 
