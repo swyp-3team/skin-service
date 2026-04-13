7598b11ac0651819dc21693cf0e8ac72eaa34957
kmsdevdata-sketch <kmsdevdata@gmail.com>
2026-04-13 17:37:35 +0900

    refactor:edit-category(#76)

--- src/main/java/com/swyp3/skin/domain/product/domain/enums/ProductCategory.java
+++ src/main/java/com/swyp3/skin/domain/product/domain/enums/ProductCategory.java
@@ -1,3 +1,13 @@
 package com.swyp3.skin.domain.product.domain.enums;
 
-public enum ProductCategory { CLEANSER, TONER, ESSENCE, SERUM, AMPOULE, LOTION, CREAM, SUNSCREEN, MASK, SPOT }
+public enum ProductCategory {
+    SKIN,
+    TONER,
+    LOTION,
+    EMULSION,
+    ESSENCE,
+    SERUM,
+    AMPOULE,
+    CREAM,
+    SUN_CARE
+}
