28dfab5148a008dc1bbb8bf016a1634d6e376360
kmsdevdata-sketch <kmsdevdata@gmail.com>
2026-04-15 20:23:09 +0900

    feat:skinresult-repository(#87)

--- src/main/java/com/swyp3/skin/domain/skinresult/repository/SkinResultRepository.java
+++ src/main/java/com/swyp3/skin/domain/skinresult/repository/SkinResultRepository.java
@@ -3,5 +3,10 @@ package com.swyp3.skin.domain.skinresult.repository;
 import com.swyp3.skin.domain.skinresult.domain.entity.SkinResult;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 public interface SkinResultRepository extends JpaRepository<SkinResult,Long> {
+
+    Optional<SkinResult> findTopByUserIdOrderByCreatedAtDesc(Long userId);
 }
