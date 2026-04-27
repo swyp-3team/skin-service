b8a95f18d790c72a34f6906cec011c70b98f767c
kmsdevdata-sketch <kmsdevdata@gmail.com>
2026-04-27 14:21:52 +0900

    fix:SecurityConfig(#122)

--- src/main/java/com/swyp3/skin/global/config/SecurityConfig.java
+++ src/main/java/com/swyp3/skin/global/config/SecurityConfig.java
@@ -35,18 +35,6 @@ public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
-    @Order(0)
-    public SecurityFilterChain actuatorChain(HttpSecurity http) throws Exception{
-        http
-                .securityMatcher("/actuator/**")
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                );
-        return http.build();
-    }
-
-    @Bean
     @Order(1)
     public SecurityFilterChain adminChain(HttpSecurity http) throws Exception {
         http
